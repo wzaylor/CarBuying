f7038ff17192884536c5e391767fd5f02c2c3e16
wzaylor <will.zaylor@gmail.com>
2015-06-24 22:12:15 -0400

    Added getData function

--- CarBuyingBackend.py
+++ CarBuyingBackend.py
@@ -5,6 +5,16 @@ def getCarMakes(dir = 'C:\\Projects\\Car_makes'):
     makes = listdir(dir)
     return makes
 
+def getCarModels(carMake, dir = 'C:\\Projects\\Car_makes', fileNames = False):
+    folder = dir + '\\' + carMake
+    models = listdir(folder)
+    for i in range(len(models)):
+        if fileNames == False:
+            models[i] = models[i].split('.')[0]
+        else:
+            models[i] = dir + '\\' + carMake + '\\' + models[i]
+    return models
+
 class getMakeData(object):
     def __init__(self, carMake, dir = 'C:\\Projects\\Car_makes'):
         self.data = {}
@@ -22,9 +32,17 @@ class getMakeData(object):
                 self.data[modelName] = cPickle.load(openedFile)
         return
 
+def _finditem(obj, key):
+    if key in obj: return obj[key]
+    for k, v in obj.items():
+        if isinstance(v,dict):
+            return _finditem(v, key)  #added return statement
+
 
 if __name__ == '__main__':
     dir = 'C:\\Projects\\Car_makes'
     makes = getCarMakes()
 
-    x = getMakeData(makes[0])
+    x = getMakeData('Honda')
+
+    #print _finditem(x.data, 'Civic')
